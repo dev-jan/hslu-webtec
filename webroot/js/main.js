480cc3f2a613a9df2686920ff5aac5f5450ff354
Jan Bucher <jan@jan-bucher.ch>
2017-10-16 10:53:05 +0200

    Implement basic navigation + define subsites (load with ajax)

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,9 +1,38 @@
 
 function toggleNavigationToolbar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+
+function loadContentPage(pagename) {
+  console.log("load page " + pagename)
+  $.ajax({
+    url: "pages/" + pagename + ".html",
+    dataType: "html",
+    success: function(data) {
+      $("#maincontent").html(data)
+      console.log("loading content successfull")
+      console.log(data)
+    },
+    error: function(error) {
+      console.log("error loading content: " + error)
+    }
+  })
+}
+
+function init() {
+  var hash = window.location.hash;
+  if (hash) {
+    loadContentPage(hash.replace("#", ""))
+  }
+  else {
+    loadContentPage('home')
+  }
+}
+$(document).ready(
+  init()
+)
